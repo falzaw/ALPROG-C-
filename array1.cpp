8131bfe0ec4a6c730cfee002a4a085197743420a
Wulanda Falza <58261013+falzaw@users.noreply.github.com>
2019-12-01 17:08:10 +0700

    Tugas 4 Alprog

---
+++ array1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int bilangan[]={10,5,1,3,5};
+	cout<<"Soal no.3 \n";
+	cout<<"\n"<<"Hasil Jumlah = "<<bilangan[0]+bilangan[1]+bilangan[2]+bilangan[3]+bilangan[4];
+	cout<<"\n"<<"Hasil Kali = "<<bilangan[0]*bilangan[1]*bilangan[2]*bilangan[3]*bilangan[4];
+}
+
