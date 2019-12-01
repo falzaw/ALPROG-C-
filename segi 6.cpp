f83586ff83e4d94a9198f13f4bcc03cb3138ce99
Wulanda Falza <58261013+falzaw@users.noreply.github.com>
2019-12-01 19:34:35 +0700

    segi enam

--- segi 6.cpp
+++ segi 6.cpp
@@ -9,12 +9,12 @@ int main(){
 		for (int j=n; j>i; j--){
 		cout<<" ";	
 		}
-		for (int k=0; k<=(2*(i+3)); k++){
+		for (int k=0; k<=(2*(i+4)); k++){
 			cout<<"*";
 		}
 		cout<<endl;
 	}
-	for (int i=0; i<=n; i++){
+	for (int i=1; i<=n; i++){
 		for (int j=0; j<i; j++){
 			cout<<" ";
 		}
